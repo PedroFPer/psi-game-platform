c37c2f3e2362795747afe1ccbd61b1a06f3f2d3d
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-02-10 10:24:38 -0300

    Tela inicial finalizada
    Adicionado menu "hamburger", animações simples nos botões e ajustes nas telas

---
+++ front_end/public/javaScript/menuHamburger.js
@@ -0,0 +1,9 @@
+function clickMenuHambur (){
+    var icon_menu_ham = document.getElementById('itens_menu_hambur');
+
+    if (icon_menu_ham.style.display === "flex") {
+        icon_menu_ham.style.display = "none";
+    } else {
+        icon_menu_ham.style.display = "flex";
+    }
+}
